31b3dbab677f4422cc3c8e6641ae5a9e1cf0745f
Lucas Theisen <ltheisen@mitre.org>
2021-10-23 18:20:49 -0400

    minor syntax fixes for templates

--- clconf/template/template_funcs_test.go
+++ clconf/template/template_funcs_test.go
@@ -9,14 +9,14 @@ import (
 func TestEscapeOsgi(t *testing.T) {
 	// quotes, double quotes, backslash, the equals sign and spaces need to be escaped
 	tests := [][]string{
-		[]string{"", ""},
-		[]string{"a", "a"},
-		[]string{"'", "\\'"},
-		[]string{"\"", "\\\""},
-		[]string{"\\", "\\\\"},
-		[]string{"=", "\\="},
-		[]string{" ", "\\ "},
-		[]string{
+		{"", ""},
+		{"a", "a"},
+		{"'", "\\'"},
+		{"\"", "\\\""},
+		{"\\", "\\\\"},
+		{"=", "\\="},
+		{" ", "\\ "},
+		{
 			"a long 'sentence' using \"some\" of the = characters",
 			"a\\ long\\ \\'sentence\\'\\ using\\ \\\"some\\\"\\ of\\ the\\ \\=\\ characters",
 		},
@@ -30,11 +30,11 @@ func TestEscapeOsgi(t *testing.T) {
 
 func TestFqdn(t *testing.T) {
 	tests := [][]string{
-		[]string{"host", "domain", "host.domain"},
-		[]string{"host.domain", "domain", "host.domain"},
-		[]string{"host.domaina", "domain", "host.domaina"},
-		[]string{"host", "subdomain.domain", "host.subdomain.domain"},
-		[]string{"host.subdomain.domain", "subdomainb.domain", "host.subdomain.domain"},
+		{"host", "domain", "host.domain"},
+		{"host.domain", "domain", "host.domain"},
+		{"host.domaina", "domain", "host.domaina"},
+		{"host", "subdomain.domain", "host.subdomain.domain"},
+		{"host.subdomain.domain", "subdomainb.domain", "host.subdomain.domain"},
 	}
 	for _, test := range tests {
 		if actual := template.Fqdn(test[0], test[1]); actual != test[2] {
@@ -45,11 +45,11 @@ func TestFqdn(t *testing.T) {
 
 func TestRegexReplace(t *testing.T) {
 	tests := [][]string{
-		[]string{".", "abc", "Z", "ZZZ"},
-		[]string{"a(.)c", "abc", "A${1}C", "AbC"},
-		[]string{"^a", "abca", "", "bca"},
-		[]string{"^abc$", "abc", "def", "def"},
-		[]string{"c", "ab\ncd", "C", "ab\nCd"},
+		{".", "abc", "Z", "ZZZ"},
+		{"a(.)c", "abc", "A${1}C", "AbC"},
+		{"^a", "abca", "", "bca"},
+		{"^abc$", "abc", "def", "def"},
+		{"c", "ab\ncd", "C", "ab\nCd"},
 	}
 	for idx, test := range tests {
 		if actual, _ := template.RegexReplace(test[0], test[1], test[2]); actual != test[3] {
